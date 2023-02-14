ab5ad652f08e0ec149c00eba6520571576e762b3
Richard Vu <rqvu.mail@gmail.com>
2023-02-13 23:19:53 -0800

    formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 
 #include <M5EPD.h>
-#include "./resources/ImageResource.h"
+
 #include "./epdgui/epdgui.h"
+#include "./resources/ImageResource.h"
 
 M5EPD_Canvas canvas(&M5.EPD);
 void Shutdown();
@@ -12,8 +13,8 @@ void setup() {
   // put your setup code here, to run once:
   M5.begin();
   M5.EPD.SetRotation(90);
-  M5.EPD.Clear(true); // Clear the screen.
-  M5.RTC.begin();     // Init the RTC.
+  M5.EPD.Clear(true);  // Clear the screen.
+  M5.RTC.begin();      // Init the RTC.
 
   char buf[100];
   sprintf(buf, "Hello World!");
@@ -23,17 +24,16 @@ void setup() {
   canvas.fillCanvas(0);
 
   canvas.drawString(buf, 50, 50);
-  canvas.pushCanvas(0, 0, UPDATE_MODE_DU4); // Update the screen.
+  canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);  // Update the screen.
 
-    for (int j = 0; j < 5; j++)
-  {
-      for (int i = 0; i < 4; i++)
-      {
-          int idx = j * 4 + i;
-          btns[idx] = new EPDGUI_Button(kTitles[idx], 12 + i * 132, 300 + j * 132, 120, 120);
-          btns[idx]->SetCustomString(kTitles[idx]);
-          EPDGUI_AddObject(btns[idx]);
-      }
+  for (int j = 0; j < 5; j++) {
+    for (int i = 0; i < 4; i++) {
+      int idx = j * 4 + i;
+      btns[idx] = new EPDGUI_Button(kTitles[idx], 12 + i * 132, 300 + j * 132,
+                                    120, 120);
+      btns[idx]->SetCustomString(kTitles[idx]);
+      EPDGUI_AddObject(btns[idx]);
+    }
   }
 
   btns[KEY_0]->AddArgs(EPDGUI_Button::EVENT_RELEASED, 0, btns[KEY_0]);
@@ -42,7 +42,7 @@ void setup() {
 
   Shutdown();
 }
- 
+
 void loop() {
   // put your main code here, to run repeatedly:
   // char buf[130];
@@ -54,17 +54,17 @@ void loop() {
 }
 
 void Shutdown() {
-    log_d("Now the system is shutting down.");
-    M5.EPD.Clear();
-    M5.EPD.WritePartGram4bpp(92, 182, 356, 300, ImageResource_logo_356x300);
-    M5.EPD.UpdateFull(UPDATE_MODE_GC16);
-    M5.EPD.UpdateFull(UPDATE_MODE_GC16);                                   
-    // SaveSetting();
-    delay(600);
-    M5.disableEPDPower();
-    M5.disableEXTPower();
-    M5.disableMainPower();
-    esp_deep_sleep_start();
-    while (1)
-        ;
+  log_d("Now the system is shutting down.");
+  M5.EPD.Clear();
+  M5.EPD.WritePartGram4bpp(92, 182, 356, 300, ImageResource_logo_356x300);
+  M5.EPD.UpdateFull(UPDATE_MODE_GC16);
+  M5.EPD.UpdateFull(UPDATE_MODE_GC16);
+  // SaveSetting();
+  delay(600);
+  M5.disableEPDPower();
+  M5.disableEXTPower();
+  M5.disableMainPower();
+  esp_deep_sleep_start();
+  while (1)
+    ;
 }
